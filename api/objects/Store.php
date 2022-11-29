4e63c51c92e1f0f5cf4cdf351427f70756d9621c
Nathan Oliveira de Santana <oliveira.nathan@aluno.ifsp.edu.br>
2022-11-29 20:54:34 -0300

    Adicionando funções CRUD a classe Store

--- api/objects/Store.php
+++ api/objects/Store.php
@@ -70,6 +70,66 @@ function create(){
       
 }
 
+// delete store
+function delete(){
+  
+    // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+  
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+  
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+  
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+  
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+  
+    return false;
+}
+
+// update the product
+function update(){
+  
+    // update query
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                address = :address,
+                cnpj = :cnpj
+            WHERE
+                id = :id";
+  
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+  
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->address=htmlspecialchars(strip_tags($this->address));
+    $this->cnpj=htmlspecialchars(strip_tags($this->cnpj));
+    $this->id=htmlspecialchars(strip_tags($this->id));
+
+  
+    // bind new values
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':address', $this->address);
+    $stmt->bindParam(':cnpj', $this->cnpj);
+    $stmt->bindParam(':id', $this->id);
+
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+  
+    return false;
+}
+
 }
 
 ?>
